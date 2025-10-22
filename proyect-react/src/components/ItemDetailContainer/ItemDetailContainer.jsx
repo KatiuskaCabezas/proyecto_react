631431c31f8f294dd43a399ee3bb1f8a74bf0cbe
Katiuska Cabezas <katiuskacabezas@gmail.com>
2025-10-22 15:19:13 -0300

    ItemDetailContainer.jsx

---
+++ proyect-react/src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,38 @@
+//Retorna de la API el detalle
+
+import { useEffect, useState } from "react"
+
+export const ItemDetailContainer= () => {
+    const [detail, setDetail] = useState({});
+
+    useEffect(() => {
+        fetch("data/products.json")
+        .then((res) => {
+            if(!res.ok){
+                throw new Error("Hubo un problema al buscar productos");                
+            } 
+            return res.json();
+        })
+
+        //Para traer el ID del producto que queramos
+        .then((data) => () => {
+            const found = data.find((p) => p.id === "1");
+            if (found) {
+                setDetail(found);
+            } else {
+                throw new Error("Producto no encontrado");
+            }
+        })
+         .catch((error) => {
+            console.log(error);            
+        });
+
+    }, []);    
+
+    return <main>
+        {Object.keys(detail).length ? (
+            <itenDettail detail={detail}/> 
+        ) : 
+        ( <p> Cargando...</p>)}
+    </main>;
+}
