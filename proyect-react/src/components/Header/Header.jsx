bfd9b73ba4a6d7542b13f4e709304bf8a7ed8d95
Katiuska Cabezas <katiuskacabezas@gmail.com>
2025-12-12 11:42:14 -0300

    logo

--- proyect-react/src/components/Header/Header.jsx
+++ proyect-react/src/components/Header/Header.jsx
@@ -1,13 +1,12 @@
 //Componete presentacional
 import { Link } from "react-router-dom";
-import { Nav } from "../Nav/Nav"
-
-import "./Header.css";
+import { Nav } from "../Nav/Nav";
 
 export const Header = () => {
-    return <header>
-        <Link to={"/"}>Logo</Link>
-        <Nav/>
-
+  return (
+    <header>
+      <Link to={"/images/logo.jpg"}>LOGO</Link>
+      <Nav />
     </header>
-}
+  );
+};
